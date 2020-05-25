4cb34146a073e9178bc24014ad5cdb071050ec40
lenovo <lenovo@NOVO-20191015XL>
2020-05-25 19:27:02 +0800

    企业微信 会话内容存档

---
+++ src/main/java/com/vocust/qywx/demo/utils/EnterpriseParame.java
@@ -0,0 +1,35 @@
+package com.vocust.qywx.demo.utils;
+
+/**
+*@author  hf
+*@version 1.0
+*@date  2020年5月15日 下午7:58:38
+*@desc 
+*/
+
+/**
+ * @see 企业微信参数
+ * @author lenovo
+ *
+ */
+public class EnterpriseParame {
+	/**
+	 *@desc 企业微信CORPID
+	 */
+	public static final String CORPID = "***REMOVED***";
+	
+	/**
+	 *@desc 企业微信SECRET
+	 */
+	public static final String SECRET = "***REMOVED***";
+	
+	/**
+	 *@desc 企业微信客户信息获取SECRET
+	 */
+	public static final String CUSTOMER_SECRET = "***REMOVED***";
+	
+	
+	
+}
+
+
